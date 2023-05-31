215693432ee9f42674ef96b44a3f6de674502e86
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-31 18:56:19 +0300

    Scroll to section 1

--- js/script.js
+++ js/script.js
@@ -7,4 +7,10 @@ btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   const horizontalOffset = window.pageXOffset;
   const verticalOffset = window.pageYOffset;
+
+  // Scroll to section 1
+  window.scrollTo(
+    s1coords.left + horizontalOffset,
+    s1coords.top + verticalOffset
+  );
 });
