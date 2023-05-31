c74e1dc307c53b5a0fb7a22f303eaddd42e2829a
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-31 19:51:01 +0300

    Create alertH1 function seperately

--- js/script.js
+++ js/script.js
@@ -13,4 +13,5 @@ btnScrollTo.addEventListener('click', function (e) {
 
 // Listen to and handle mouse enter events
 const h1 = document.querySelector('h1');
-h1.addEventListener('mouseenter', () => alert('Get off me!'));
+const alertH1 = () => alert('Get off me!');
+h1.addEventListener('mouseenter', alertH1);
