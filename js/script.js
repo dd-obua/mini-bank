f9a734de46478fd5357b5baff84a6f9dd2c09994
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-31 19:55:10 +0300

    Remove event listener (listen to event only once)

--- js/script.js
+++ js/script.js
@@ -15,5 +15,7 @@ btnScrollTo.addEventListener('click', function (e) {
 const h1 = document.querySelector('h1');
 const alertH1 = function () {
   alert('Get off me!');
+  h1.removeEventListener('mouseenter', alertH1);
 };
+
 h1.addEventListener('mouseenter', alertH1);
