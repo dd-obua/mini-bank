7f8ebd06acc865edf7e703cfcefea50f59bebc2b
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-01 18:36:48 +0300

    Add comment

--- js/traverse-dom.js
+++ js/traverse-dom.js
@@ -4,6 +4,6 @@ const select = (element, selector) => element.querySelector(selector);
 
 const h1 = select(document, 'h1');
 
+// Select and manipulate child elements
 console.log(h1.querySelectorAll('.highlight'));
 console.log(h1.childNodes);
-console.log(h1.children);
