30d06ab86be118263fc40d44ac1c02a6f32ea335
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-01 18:21:36 +0300

    Log all direct children of h1

--- js/traverse-dom.js
+++ js/traverse-dom.js
@@ -5,3 +5,4 @@ const select = (element, selector) => element.querySelector(selector);
 const h1 = select(document, 'h1');
 
 console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
