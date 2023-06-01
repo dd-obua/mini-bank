05005b81afa7659c5d0f42d965109baf75b7837c
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-01 15:44:32 +0300

    Implement smooth scrolling using event delegation

--- js/script.js
+++ js/script.js
@@ -1,51 +1,25 @@
 'use strict';
 
+// Create selectors
 const select = selector => document.querySelector(selector);
 const selectAll = selector => document.querySelectorAll(selector);
 
+// Select elements
 const btnScrollTo = select('.btn--scroll-to');
 const section1 = select('#section--1');
+const navLinks = select('.nav__links');
 
-btnScrollTo.addEventListener('click', function (e) {
-  const s1coords = section1.getBoundingClientRect();
-  const horizontalOffset = window.pageXOffset;
-  const verticalOffset = window.pageYOffset;
-
+// Scroll smoothly to section 1 on clicking learn more button
+btnScrollTo.addEventListener('click', e => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Listen to and handle mouse enter events
-const h1 = select('h1');
+// Apply events delegation
+navLinks.addEventListener('click', function (e) {
+  e.preventDefault();
 
-const alertH1 = function () {
-  alert('Get off me!');
-};
-
-h1.addEventListener('mouseenter', alertH1);
-
-setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
-
-// Implement event propagation (esp. event bubbling)
-const rdmInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const rdmCol = () =>
-  `rgb(${rdmInt(0, 255)},${rdmInt(0, 255)},${rdmInt(0, 255)})`;
-
-select('.nav__link').addEventListener('click', function (e) {
-  this.style.background = rdmCol();
-  console.log('link', e.target, e.currentTarget);
-  console.log(this === e.currentTarget);
-});
-
-select('.nav__links').addEventListener('click', function (e) {
-  this.style.background = rdmCol();
-  console.log('container', e.target, e.currentTarget);
-  console.log(this === e.currentTarget);
-
-  e.stopPropagation();
-});
-
-select('.nav').addEventListener('click', function (e) {
-  this.style.background = rdmCol();
-  console.log('nav', e.target, e.currentTarget);
-  console.log(this === e.currentTarget);
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    select(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
