36cfab989623a64c787aae67e71af671c4449046
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-01 19:00:28 +0300

    Adjust code spacing

--- js/traverse-dom.js
+++ js/traverse-dom.js
@@ -14,10 +14,10 @@ console.log(h1.lastElementChild);
 h1.firstElementChild.style.color = '#fff';
 h1.lastElementChild.style.color = '#000';
 console.log();
+
 // Select and manipulate parents
 console.log(h1.parentNode);
 console.log(h1.parentElement);
-
 console.log(h1.closest('.header'));
 h1.closest('.header').style.background = 'var(--gradient-primary)';
 h1.closest('h1').style.background = 'var(--gradient-secondary)';
