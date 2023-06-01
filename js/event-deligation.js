33fd469bee3e013ebb07adb5f8eecdac71a7d632
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-01 14:24:08 +0300

    Select scroll-to buton and section 1

--- js/event-deligation.js
+++ js/event-deligation.js
@@ -3,3 +3,7 @@
 // Create selectors
 const select = selector => document.querySelector(selector);
 const selectAll = selector => document.querySelectorAll(selector);
+
+// Select elements
+const btnScrollTo = select('.btn--scroll-to');
+const section1 = select('#section--1');
