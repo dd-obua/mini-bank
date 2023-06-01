24befea7a5e6d0c8737de74ecc857deed3ec5783
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-01 15:26:43 +0300

    Seperately select nav__links

--- js/event-deligation.js
+++ js/event-deligation.js
@@ -7,6 +7,7 @@ const selectAll = selector => document.querySelectorAll(selector);
 // Select elements
 const btnScrollTo = select('.btn--scroll-to');
 const section1 = select('#section--1');
+const navLinks = select('.nav__links');
 
 // Scroll smoothly to section 1 on clicking learn more button
 btnScrollTo.addEventListener('click', e => {
@@ -14,7 +15,7 @@ btnScrollTo.addEventListener('click', e => {
 });
 
 // Apply events delegation
-select('.nav__links').addEventListener('click', function (e) {
+navLinks.addEventListener('click', function (e) {
   e.preventDefault();
   const id = e.target.getAttribute('href');
   select(id).scrollIntoView({ behavior: 'smooth' });
